19dfdf377289f3901dadd251ccf84c60d06bae76
Nestor Masinde <nestormasinde@gmail.com>
2023-12-18 22:52:18 +0300

    Array Filter Method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, search){
+    return drivers.filter(driver => driver.toLowerCase().includes(search.toLowerCase()));
+
+}
+
+function fuzzyMatch(drivers, search){
+    return drivers.filter(driver => driver.toLowerCase().startsWith(search.toLowerCase()));
+}
+
+function matchName(drivers, search){
+    return drivers.filter(driver => driver.name.toLowerCase() === search.toLowerCase());
+}
